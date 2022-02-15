d34c7599a86f88e142772e8ff13be762dc595b65
autocode-test-contributor-2 <autocode.test.contributor2@yandex.ru>
2022-02-15 07:58:11 +0000

    Update spec/calculator_spec.rb, spec/super/super_calc_spec.rb

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -4,6 +4,9 @@ require 'rspec'
 require_relative '../lib/calculator'
 
 describe Calculator do
+
+# Student test repository
+
   before do
     @calculator = Calculator.new('RSpec calculator')
   end
@@ -12,16 +15,4 @@ describe Calculator do
     expect(@calculator.add(2, 15)).to eq 17
   end
 
-  it 'should subtract 2 numbers correctly' do
-    expect(@calculator.subtract(33, 55)).to eq(-22)
-  end
-
-  it 'should divide 2 numbers correctly' do
-    expect(@calculator.divide(4, 2)).to eq 2
-    expect(@calculator.divide(1, 0)).to eq 0
-  end
-
-  it 'should multiply 2 numbers correctly' do
-    expect(@calculator.multiply(3, 5)).to eq(15)
-  end
 end
--- spec/super/super_calc_spec.rb
+++ spec/super/super_calc_spec.rb
@@ -12,7 +12,4 @@ describe SuperCalculator do
     expect(@super_calculator.add_super(1, 2, 3)).to eq 6
   end
 
-  it 'should multiply 3 numbers correctly' do
-    expect(@super_calculator.multiply_super(4, 5, 6)).to eq 120
-  end
 end
