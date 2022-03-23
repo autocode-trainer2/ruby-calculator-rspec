bc8394e9bbe666931aa03b3c1f0d734124cba614
Kirill Peshin <kirill_peshin@epam.com>
2022-03-23 09:40:21 +0300

    coverage

--- spec/calculator_spec.rb
+++ spec/calculator_spec.rb
@@ -12,13 +12,8 @@ describe Calculator do
     expect(@calculator.add(2, 15)).to eq 17
   end
 
-  it 'should subtract 2 numbers correctly' do
-    expect(@calculator.subtract(33, 55)).to eq(-22)
-  end
-
   it 'should divide 2 numbers correctly' do
     expect(@calculator.divide(4, 2)).to eq 2
-    expect(@calculator.divide(1, 0)).to eq 0
   end
 
   it 'should multiply 2 numbers correctly' do
--- spec/super/super_calc_spec.rb
+++ spec/super/super_calc_spec.rb
@@ -8,10 +8,6 @@ describe SuperCalculator do
     @super_calculator = SuperCalculator.new('RSpec Super calculator')
   end
 
-  it 'should add 3 numbers correctly' do
-    expect(@super_calculator.add_super(1, 2, 3)).to eq 6
-  end
-
   it 'should multiply 3 numbers correctly' do
     expect(@super_calculator.multiply_super(4, 5, 6)).to eq 120
   end
